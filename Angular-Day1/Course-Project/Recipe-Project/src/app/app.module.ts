6ecc5d760fcddcbe259c7fd4e497394a6c9ab843
Abhinav <raiabhinav1995@gmail.com>
2020-04-28 13:25:53 +0530

    Some more conccepts
    Some more conccepts

---
+++ Angular-Day1/Course-Project/Recipe-Project/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HeaderComponent} from './Header/header.component'
+//import { fromEventPattern } from 'rxjs';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
